d2020b1a50ef8bc338054cca9fbb390013f6a2d1
Huy Hoang Phan <hoangph271@gmail.com>
2021-04-26 04:39:36 +0700

    [Fix] Stream response with mime & headers

--- src/responders/stream_responder.rs
+++ src/responders/stream_responder.rs
@@ -1,14 +1,11 @@
-use super::super::{
-    request_parsers::RangeFromHeader,
-    app_context::{bins}
-};
-use rocket::http::{ContentType, Status};
+use super::super::{app_context::bins, request_parsers::RangeFromHeader};
+use rocket::http::{Header, Status};
 use rocket::request::Request;
-use rocket::response::{self, Responder, Stream, Redirect};
+use rocket::response::{self, Redirect, Responder};
 use rocket::Response;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{Cursor, SeekFrom};
+use std::io::{SeekFrom};
 use std::path::PathBuf;
 
 pub struct StreamResponder {
@@ -24,32 +21,46 @@ impl StreamResponder {
 impl<'a> Responder<'a> for StreamResponder {
     fn respond_to(self, req: &Request) -> response::Result<'a> {
         let file_path = bins().join(self.path.to_owned());
-        let file = File::open(file_path);
+        let file = File::open(file_path.clone());
 
         if let Err(_) = file {
-            let message = format!("{:?}", self.path);
-            return Response::build()
-                .status(Status::NotFound)
-                .header(ContentType::Plain)
-                .sized_body(Cursor::new(message))
+            return Err(Status::NotFound);
+        }
+
+        let mut file = file.map_err(|_| Status::BadRequest)?;
+        let file_len = file.metadata().map_err(|_| Status::BadRequest)?.len();
+
+        let (start, end) = match self.range {
+            RangeFromHeader::Nope => {
+                let request_path = format!(
+                    "/bins/{}",
+                    self.path.to_string_lossy().to_owned().to_string()
+                );
+                return Redirect::to(request_path).respond_to(req);
+            }
+            RangeFromHeader::OpenEnd(start) => {
+                (start, file_len as usize)
+            }
+            RangeFromHeader::ClosedEnd(start, end) => (start, end),
+        };
+
+        if let Ok(_) = file.seek(SeekFrom::Start(start as u64)) {
+            let mime = mime_guess::from_path(self.path).first();
+            let mut response = Response::build();
+
+            if let Some(mime) = mime {
+                response.header(Header::new("Content-Range", format!("bytes {}-{}/{}", start, end, file_len)));
+                response.header(Header::new("Accept-Ranges", "bytes"));
+                response.header(Header::new("Content-Length", (end - start).to_string()));
+                response.header(Header::new("Content-Type", String::from(mime.as_ref())));
+            }
+
+            return response
+                .header(Header::new("Accept-Ranges", "bytes"))
+                .streamed_body(file.take((end - start) as u64))
                 .ok();
         }
 
-        let mut file = file.unwrap();
-
-        if let RangeFromHeader::OpenEnd(start) = self.range {
-            if let Ok(_) = file.seek(SeekFrom::Start(start as u64)) {
-                return Stream::from(file).respond_to(req);
-            }
-        }
-
-        if let RangeFromHeader::ClosedEnd(start, end) = self.range {
-            if let Ok(_) = file.seek(SeekFrom::Start(start as u64)) {
-                return Stream::from(file.take(end as u64)).respond_to(req);
-            }
-        }
-
-        let request_path = format!("/bins/{}", self.path.to_string_lossy().to_owned().to_string());
-        return Redirect::to(request_path).respond_to(req);
+        Err(Status::BadRequest)
     }
 }
