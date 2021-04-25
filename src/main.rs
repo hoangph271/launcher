eec82bab4957a47d4a5d57b79691733d28ba647d
Huy Hoang Phan <hoangph271@gmail.com>
2021-04-26 05:28:30 +0700

    [Add] WIP - GET /dirs with JSON response

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,95 @@ use app_context::{bins, init_app};
 use request_parsers::RangeFromHeader;
 use responders::stream_responder::StreamResponder;
 use rocket_contrib::serve::StaticFiles;
+use serde::Serialize;
 use std::path::PathBuf;
+use std::time::UNIX_EPOCH;
 
+#[derive(Debug, Serialize)]
+struct FSEntry {
+    key: String,
+    is_dir: bool,
+    children: Option<Vec<FSEntry>>,
+    size: Option<u64>,
+    mime: Option<String>,
+    created: u128,
+    modified: u128,
+}
+
+fn extract_timestamps(metadata: &std::fs::Metadata) -> (u128, u128) {
+    let created = metadata
+        .created()
+        .unwrap()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+    let modified = metadata
+        .modified()
+        .unwrap()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+
+    (created, modified)
+}
+fn read_file_entry(path: PathBuf) -> FSEntry {
+    let file_path = bins().join(path.to_owned());
+    let file = std::fs::File::open(file_path).unwrap();
+    let metadata = file.metadata().unwrap();
+
+    let mime = mime_guess::from_path(path.to_owned()).first();
+    let (created, modified) = extract_timestamps(&metadata);
+
+    FSEntry {
+        key: path.to_string_lossy().as_ref().to_string(),
+        is_dir: false,
+        children: None,
+        mime: if let Some(mime) = mime {
+            Some(mime.as_ref().to_string())
+        } else {
+            None
+        },
+        size: Some(metadata.len()),
+        created,
+        modified,
+    }
+}
 #[get("/<path..>")]
-fn dirs(path: PathBuf) -> String {
-    bins()
-        .join(path)
-        .as_os_str()
-        .to_string_lossy()
-        .to_owned()
-        .to_string()
+fn dirs(path: PathBuf) -> rocket_contrib::json::Json<FSEntry> {
+    use std::fs::File;
+    let item_path = bins().join(path.to_owned());
+    let file = File::open(item_path.to_owned())
+        .map_err(|e| {
+            // TODO: Handle in seperate Responder...?
+            println!("{:?}", e);
+        })
+        .unwrap();
+    let metadata = file.metadata().unwrap();
+
+    if metadata.is_file() {
+        return rocket_contrib::json::Json(read_file_entry(path));
+    }
+
+    let children = std::fs::read_dir(item_path.to_owned())
+        .unwrap()
+        .map(|entry| {
+            read_file_entry(entry.unwrap().path().strip_prefix(bins()).unwrap().to_path_buf())
+        })
+        .collect::<Vec<FSEntry>>();
+    let (created, modified) = extract_timestamps(&metadata);
+
+    rocket_contrib::json::Json(FSEntry {
+        key: path.to_string_lossy().as_ref().to_string(),
+        is_dir: metadata.is_dir(),
+        children: Some(children),
+        size: None,
+        mime: None,
+        created,
+        modified,
+    })
 }
 #[get("/")]
-fn dirs_index() -> String {
+fn dirs_index() -> rocket_contrib::json::Json<FSEntry> {
     dirs(PathBuf::from(""))
 }
 
