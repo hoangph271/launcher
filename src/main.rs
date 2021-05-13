182bc862256f4365b9e9d01b932321870385de23
Huy Hoang Phan <hoangph271@gmail.com>
2021-05-13 15:21:27 +0700

    [Add] Tests to main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,7 @@ use app_context::{bins, init_app};
 use rocket_contrib::serve::StaticFiles;
 use routers::{auths, dirs, others, streams, users};
 
-fn main() {
-    init_app();
-
+fn launch_app() -> rocket::Rocket {
     rocket::ignite()
         .mount("/bins", StaticFiles::from(bins()))
         .mount(
@@ -44,5 +42,56 @@ fn main() {
             others::unprocessable_entity
         ])
         .attach(others::cors::Cors)
-        .launch();
+}
+
+fn main() {
+    init_app();
+    launch_app().launch();
+}
+
+#[cfg(test)]
+#[macro_use]
+extern crate diesel_migrations;
+
+#[cfg(test)]
+mod main_tests {
+    use super::*;
+    use rocket::http::Status;
+    use rocket::local::Client;
+
+    #[test]
+    fn it_works() {}
+
+    #[test]
+    fn can_use_memory_db() {
+        embed_migrations!("./dal/migrations");
+
+        use dal::establish_connection;
+        use diesel::prelude::*;
+        use std::env;
+
+        env::set_var("DATABASE_URL", ":memory:");
+        let conn = establish_connection();
+        conn.execute(
+            "SELECT 
+            name
+        FROM 
+            sqlite_master 
+        WHERE 
+            type ='table' AND 
+            name NOT LIKE 'sqlite_%';",
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn get_non_exists_route() {
+        let app = launch_app();
+        let client = Client::new(app).unwrap();
+
+        let mut res = client.get("/418").dispatch();
+
+        assert_eq!(res.status(), Status::ImATeapot);
+        assert_eq!(res.body_string(), Some(String::from("418 | I'm a teapot")));
+    }
 }
